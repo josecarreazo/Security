7f903170e2cbced0bced1e3a48918b59859f7690
unknown <josecarreazosaldarriaga@gmail.com>
2020-08-30 21:18:25 -0500

    md5-version

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
 let shown=null;
 
 
@@ -22,7 +22,6 @@ const userSchema= new mongoose.Schema({
     password: String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.ENCKEY, encryptedFields: ['password'] });
 const User=new mongoose.model("User", userSchema)
 
 
@@ -41,7 +40,7 @@ app.get("/register", function(req,res){
 app.post("/register", function(req,res){
     const newUser=new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     newUser.save(function(error){
         if(!error){
